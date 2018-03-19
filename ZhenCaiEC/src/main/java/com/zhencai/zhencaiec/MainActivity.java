e412061650dd1767da1a053a0b55f4dded5849c5
573955423@qq.com <15918453419a>
2018-03-19 16:19:33 +0800

    init

--- ZhenCaiEC/src/main/java/com/zhencai/zhencaiec/MainActivity.java
+++ ZhenCaiEC/src/main/java/com/zhencai/zhencaiec/MainActivity.java
@@ -7,6 +7,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
     }
+
+
 }
