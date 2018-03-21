95a122ac6b71a36a8dafd7ade3e467d0202854d9
573955423@qq.com <15918453419a>
2018-03-21 10:10:28 +0800

    fdgdfgdfg

---
+++ ZhenCaiEC/src/main/java/com/zhencai/zhencaiec/ZhenCaiApp.java
@@ -0,0 +1,10 @@
+package com.zhencai.zhencaiec;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2018/3/21 0021.
+ */
+
+public class ZhenCaiApp extends Application {
+}
---
+++ zhencai-core/src/main/java/com/zhencai/zhencai/app/ZhenCai.java
@@ -0,0 +1,8 @@
+package com.zhencai.zhencai.app;
+
+/**
+ * Created by Administrator on 2018/3/19 0019.
+ */
+
+public class ZhenCai {
+}
